c788c7dfcb7a5ff26314254c06ee96176138c30a
Rafael Dohms <rdohms@gmail.com>
2015-11-15 21:50:11 +0100

    Updating readme and publish code

--- publish.sh
+++ publish.sh
@@ -9,5 +9,5 @@ if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
 # Add --delete right before "output_prod" to have rsync remove files that are
 # deleted locally from the destination too. See README.md for an example.
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
+rsync -avze 'ssh -p 22' output_prod/ sonic.doh.ms:/var/www/shirtsoftheelephpant.com
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
